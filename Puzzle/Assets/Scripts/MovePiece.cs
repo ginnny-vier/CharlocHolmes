ebdabac9004f874336d60324f2d2386bb88c3b08
Ginny <virge.vier@gmail.com>
2022-10-14 16:48:24 +0200

    background

--- Puzzle/Assets/Scripts/MovePiece.cs
+++ Puzzle/Assets/Scripts/MovePiece.cs
@@ -7,10 +7,17 @@ public class MovePiece : MonoBehaviour
     // Start is called before the first frame update
 public GameObject selectedObject;
     Vector3 offset;
+    private Rigidbody2D rb;
+
+    public string pieceStatus = "";
+
+    void Start()
+    {
+
+    }
     void Update()
     {
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Debug.Log(mousePosition);
         if (Input.GetMouseButtonDown(0))
         {
             Collider2D targetObject = Physics2D.OverlapPoint(mousePosition);
@@ -27,6 +34,18 @@ public GameObject selectedObject;
         if (Input.GetMouseButtonUp(0) && selectedObject)
         {
             selectedObject = null;
+        }
+
+        void OnTriggerEnter2D(Collider2D other)
+        {   
+            if (other.gameObject.name == gameObject.name+1)
+
+                {
+                    transform.position = other.gameObject.transform.position;
+                    pieceStatus = "Locked";
+                    Debug.Log(pieceStatus);
+
+                }
         }
     }
 }
